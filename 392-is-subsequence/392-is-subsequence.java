69aeeb7efcb8789043996d4e6c79d40e7f9b95f9
Ayush Kr. Pathak <104672642+payush17@users.noreply.github.com>
2022-08-09 15:16:45 +0530

    Time: 1 ms (92.94%), Space: 42.4 MB (19.00%) - LeetHub

--- 392-is-subsequence/392-is-subsequence.java
+++ 392-is-subsequence/392-is-subsequence.java
@@ -1,49 +1,11 @@
 class Solution {
-        public int binSearchHelper(List<Integer> idxList, int lastFound){
-        int start=0;
-        int end = idxList.size()-1;
-        int result = -1;
-        while(start<=end){
-            int mid = start + (end-start)/2;
-            if(idxList.get(mid)>lastFound){
-                result = idxList.get(mid);
-                end = mid-1;
-            } else {
-                start = mid+1;
-            }
-            
+    public boolean isSubsequence(String s, String t) {
+        int j=0;
+        for(int i=0;i<t.length() && j< s.length();i++ ){
+            if(t.charAt(i)==s.charAt(j))
+                j++;
         }
-            return result;
-      
-}
-   public boolean isSubsequence(String s, String t) {
-        if(s==null || t==null){
-            return false;
-        }
-        Map<Character, List<Integer>> map = new HashMap();
-        
-        for(int i=0; i<t.length();i++){
-            List<Integer> idx = map.getOrDefault(t.charAt(i),new ArrayList());
-            idx.add(i);
-            map.put(t.charAt(i), idx);
-        }
-        
-        int lastFound = -1;
-        for(int i=0; i<s.length(); i++) {
-            if(!map.containsKey(s.charAt(i))){
-                return false;
-            }
-            
-            List<Integer> idxList = map.get(s.charAt(i));
-            int idx = binSearchHelper(idxList, lastFound);
-            if(idx ==-1){
-                return false;
-            }
-            lastFound = idx;
-        }
-        
-        return true;
+        return (j== s.length());
+        //return false;
     }
-
-    
 }
