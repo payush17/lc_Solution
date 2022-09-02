aa338e6f278f0f0a25d63bb0b48696a3749e6e2a
Ayush Kr. Pathak <104672642+payush17@users.noreply.github.com>
2022-09-02 13:55:11 +0530

    Time: 46 ms (69.46%), Space: 8.8 MB (79.08%) - LeetHub

--- 1387-sort-integers-by-the-power-value/1387-sort-integers-by-the-power-value.cpp
+++ 1387-sort-integers-by-the-power-value/1387-sort-integers-by-the-power-value.cpp
@@ -1,36 +1,26 @@
 class Solution {
-
-    public int getKth(int low, int hi, int k) {
-        List<Pair> power = new ArrayList<>();
-    
-        while(low<=hi){
-            int x = low;
-            int count =0;
-            while(x!=1){
-                if(x%2 == 0){
-                    x /= 2;
-                    count++;}
-                else{
-                    x = 3*x +1;
-                    count++;
-                }
-            }
-          power.add(new Pair(low,count) );
-            low++;
-            
-        }
-        Collections.sort(power,(a, b) -> a.pow - b.pow) ;
-        return power.get(k-1).n;
-        
-        
-    }
-}
-public class Pair
-{
-  int n ;int  pow;
-    public  Pair(int  n, int pow)
-  {
-    this.n = n;
-    this.pow = pow;
-  }
-}
+public:
+	int getKth(int lo, int hi, int k) {
+		vector<pair<int, int>>v;
+		for (int i = lo; i <= hi; i++)
+		{
+			int ans = 0;
+			int x = i;
+			while (x != 1)
+			{
+				if (x % 2)
+				{
+					x = 3 * x + 1;
+				}
+				else
+				{
+					x = x / 2;
+				}
+				ans++;
+			}
+			v.push_back({ans, i});
+		}
+		sort(v.begin(), v.end());
+		return v[k - 1].second;
+	}
+};
