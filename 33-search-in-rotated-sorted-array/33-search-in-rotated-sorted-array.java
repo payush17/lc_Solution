e6176b97bdb4fa1edd90eb5034136c3668c30ce1
Ayush Kr. Pathak <104672642+payush17@users.noreply.github.com>
2022-09-05 17:35:33 +0530

    Time: 0 ms (100.00%), Space: 41.9 MB (89.31%) - LeetHub

--- 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.java
+++ 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.java
@@ -1,20 +1,40 @@
 class Solution {
-    public int search(int[] arr, int x) {
-       int l = 0, h = arr.length - 1;
-        while (l <= h) {
-            int mid = l + (h-l)/2;
-            if(arr[mid]==x)return mid;
-            
-            if(arr[l]<=arr[mid]){
-                if(x>=arr[l] && x<arr[mid])h=mid-1;
-                else l=mid+1;
+    public int search(int[] nums, int target) {
+         int l = 0;
+        int r = nums.length - 1;
+        
+        while(l <= r)
+        {
+            int mid = l + (r-l) / 2;
+            if (nums[mid] == target)
+                return mid;
+			// with duplicates we can have this contdition, just update left & right
+            if((nums[l] == nums[mid]) && (nums[r] == nums[mid]))
+            {
+                l++;
+                r--;
             }
-            else{
-                if(x>arr[mid] && x<=arr[h])l=mid+1;
-                 else h=mid-1;
+			// first half
+			// first half is in order
+            else if(nums[l] <= nums[mid])
+            {
+				// target is in first  half
+                if((nums[l] <= target) && (nums[mid] > target))
+                    r = mid - 1;
+                else
+                    l = mid + 1;
+            }
+			// second half
+			// second half is order
+			// target is in second half
+            else
+            {
+                if((nums[mid] < target) && (nums[r]>= target))
+                    l = mid + 1;
+                else
+                    r = mid - 1;
             }
         }
-            return -1;
-
+        return -1;
     }
 }
