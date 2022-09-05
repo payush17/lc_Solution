c6637f5cf0e5955642001ffed07b748ec2c0cce5
Ayush Kr. Pathak <104672642+payush17@users.noreply.github.com>
2022-09-05 17:13:11 +0530

    Time: 69 ms (71.75%), Space: 90.1 MB (66.23%) - LeetHub

---
+++ 2064-minimized-maximum-of-products-distributed-to-any-store/2064-minimized-maximum-of-products-distributed-to-any-store.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int minimizedMaximum(int threshold, int[] nums) {
+        int low = 1;
+        int high = nums[0];
+        for(int x : nums)
+            high = Math.max(high,x);
+
+        int min_divisor = high;
+        while (low < high) {
+            int k = low + (high - low) / 2;
+            int curr_ans = 0;
+            for (int x : nums)
+                curr_ans += (x+ k-1)/k ; 
+// v += (n + mid - 1)/ mid ;
+            if (curr_ans <= threshold) {
+                min_divisor = Math.min(min_divisor, k);
+                high = k;
+            } 
+            else low = k + 1;
+        }
+
+        return min_divisor; 
+    }
+}
