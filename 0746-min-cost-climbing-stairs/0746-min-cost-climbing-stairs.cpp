3a395d91150e121a47c17dc3b8d3ff4ed6744075
Ayush Kr. Pathak <104672642+payush17@users.noreply.github.com>
2022-10-15 09:22:07 +0530

    Time: 11 ms (64.40%), Space: 13.9 MB (31.09%) - LeetHub

--- 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.cpp
+++ 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.cpp
@@ -7,7 +7,7 @@ class Solution
 
             dp[0] = 0;
             dp[1] = 0;
-            for (int i = 2; i < dp.size(); i++)
+            for (int i = 2; i < cost.size()+1; i++)
             {
 
                 dp[i] = min(cost[i - 1] + dp[i - 1], cost[i - 2] + dp[i - 2]);
