641a0e30a7f12dd00dd2388ddc518fcc64922dfc
Ayush Kr. Pathak <104672642+payush17@users.noreply.github.com>
2022-10-23 10:12:58 +0530

    Time: 89 ms (40.89%), Space: 30.9 MB (21.22%) - LeetHub

---
+++ 0645-set-mismatch/0645-set-mismatch.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    vector<int> findErrorNums(vector<int>& nums) {
+        unordered_map<int,int> map;
+    vector<int> ans;
+    for(int i=0;i<nums.size();i++){
+        map[nums[i]]++;
+    }
+    for(auto it : map){
+      if(it.second == 2)
+         ans.push_back( it.first);
+    }
+    for(int i=1;i<nums.size()+1;i++){
+      if(map[i]==0)
+          ans.push_back(i);
+    }   
+    return ans;
+    }
+    
+};
