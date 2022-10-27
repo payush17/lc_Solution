50e1043b9e05990793f6cb29b4a06591245fa91c
Ayush Kr. Pathak <104672642+payush17@users.noreply.github.com>
2022-10-27 11:53:39 +0530

    Time: 0 ms (100.00%), Space: 6.2 MB (52.02%) - LeetHub

--- 1945-sum-of-digits-of-string-after-convert/1945-sum-of-digits-of-string-after-convert.cpp
+++ 1945-sum-of-digits-of-string-after-convert/1945-sum-of-digits-of-string-after-convert.cpp
@@ -1,37 +1,40 @@
 class Solution
 {
     public:
-
-        int getLucky(string s, int k)
-        {
-            long long int num = 0;
-            long long int res = 0;
-            long int y = 0;
-
-            for (auto x: s)
-            {
-
-                int temp = x - 'a' + 1;
-                if (temp >= 10)
-                {
-                    num += temp % 10;
-                    num += temp / 10;
-                }
-                else
-                    num += temp;
-            }
-
-            for (int i = 2; i <= k; i++)
-            {
-                int sum = 0;
-
-                while (num)
-                {
-                    sum += num % 10;
-                    num /= 10;
-                }
-                num = sum;
-            }
-            return num;
+    int digit(int n){
+        int s=0;
+        while(n>0){
+            s += n%10;
+            n /= 10;
         }
+        return s;
+    }
+        
+
+    int getLucky(string s, int k)
+    {
+        long long int num = 0;
+        long long int res = 0;
+        long int y = 0;
+
+        for (auto x: s)
+        {
+
+            int temp = x - 'a' + 1;
+            if (temp >= 10)
+            {
+                num += temp % 10;
+                num += temp / 10;
+            }
+            else
+                num += temp;
+        }
+        cout<<num;
+        res = num;
+        while(k>1){
+           res = digit(res);
+            --k;
+        }
+      return res;
+    }
 };
