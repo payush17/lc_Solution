f74e489559aca9bbd1f6e3d8c8d202a37502d223
Ayush Kr. Pathak <104672642+payush17@users.noreply.github.com>
2022-10-27 18:28:04 +0530

    Time: 8 ms (94.23%), Space: 7.5 MB (87.50%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
@@ -1,21 +1,20 @@
-class Solution {
-public:
-    int lengthOfLongestSubstring(string s) {
-        if(s.size()<2)return s.size();
-        int left =0;
-        int right = 0;
-        int maxLen =0;
-        unordered_set<char> set;
-        while(left < s.size() && right < s.size()){
-            
-            if(set.find(s[right])==set.end()){
-                set.insert(s[right++]);
-                maxLen = max(maxLen,right-left);
+class Solution
+{
+    public:
+        int lengthOfLongestSubstring(string s)
+        {
+
+            vector<int> a(128, -1);
+            int maxLen = 0, l = 0, r = 0;
+            while (r < size(s))
+            {
+                if (a[s[r]] == -1 || a[s[r]]<l  )	 
+                    maxLen = max(maxLen, r - l + 1);
+                else{
+                    l = a[s[r]] + 1;
+                }                       
+                a[s[r++]] = r;	
             }
-            else
-                set.erase(s[left++]);
-            
+            return maxLen;
         }
-        return maxLen;
-    }
 };
