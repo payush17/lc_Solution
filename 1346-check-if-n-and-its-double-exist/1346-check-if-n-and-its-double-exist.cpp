9db38fa0fa6497f2f812f89a1e1f7f70df6db0de
Ayush Kr. Pathak <104672642+payush17@users.noreply.github.com>
2022-11-03 13:47:41 +0530

    Time: 10 ms (77.57%), Space: 10.5 MB (28.07%) - LeetHub

--- 1346-check-if-n-and-its-double-exist/1346-check-if-n-and-its-double-exist.cpp
+++ 1346-check-if-n-and-its-double-exist/1346-check-if-n-and-its-double-exist.cpp
@@ -1,16 +1,18 @@
 class Solution {
 public:
+    
     bool checkIfExist(vector<int>& arr) {
-        
+        set<int> set;
+        string s= "ankit";
+        cout<<s.length();
         for(int i=0;i<arr.size();i++){
-            for(int j=0;j<arr.size();j++){
-                if(i!=j)
-                if(arr[i]== 2*arr[j]){
-                    
-                    return true;
-                }
-            }
+            if(set.count( 2 *arr[i]) || !(arr[i]%2) && (set.count(arr[i]/2))  )
+               return true;
+                
+                
+                set.insert(arr[i]);
         }
         return false;
+      
     }
 };
