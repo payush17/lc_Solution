1df25fdf8e17f044cb4ca41f015897b86639eae5
Ayush Kr. Pathak <104672642+payush17@users.noreply.github.com>
2022-11-15 15:31:44 +0530

    Time: 51 ms (74.40%), Space: 31 MB (9.34%) - LeetHub

--- 0222-count-complete-tree-nodes/0222-count-complete-tree-nodes.cpp
+++ 0222-count-complete-tree-nodes/0222-count-complete-tree-nodes.cpp
@@ -1,22 +1,37 @@
 /**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
+ *Definition for a binary tree node.
+ *struct TreeNode {
+ *    int val;
+ *    TreeNode * left;
+ *    TreeNode * right;
+ *    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ *};
  */
-class Solution {
-public:
-    int countNodes(TreeNode* root) {
-        if(root==NULL)return 0;
-        int left = countNodes(root->left);
-        int right = countNodes(root->right);
-        
-       int total=1+left+right;
-        return total;
-    }
+class Solution
+{
+    public:
+        int countNodes(TreeNode *root)
+        {
+            if (root == NULL) return 0;
+            int leftcnt = 0;
+            int rightcnt = 0;
+            TreeNode *lefttree = root;
+            TreeNode *righttree = root;
+            while (lefttree != NULL)
+            {
+                leftcnt++;
+                lefttree = lefttree->left;
+            }
+            while (righttree != NULL)
+            {
+                rightcnt++;
+                righttree = righttree->right;
+            }
+            if (leftcnt == rightcnt)
+                return pow(2, leftcnt) - 1;
+
+            return 1+ countNodes(root->left) + countNodes(root->right) ;
+        }
 };
