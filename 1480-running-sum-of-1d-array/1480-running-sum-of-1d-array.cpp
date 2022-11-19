2d5ffb305dd358f1d5aefc32a86df995bbb57475
Ayush Kr. Pathak <104672642+payush17@users.noreply.github.com>
2022-11-19 19:44:02 +0530

    Time: 9 ms (28.20%), Space: 8.7 MB (37.95%) - LeetHub

--- 1480-running-sum-of-1d-array/1480-running-sum-of-1d-array.cpp
+++ 1480-running-sum-of-1d-array/1480-running-sum-of-1d-array.cpp
@@ -4,8 +4,8 @@ public:
         vector<int> run_sum;
         run_sum.push_back(nums[0]);
         for(int i=1;i<size(nums);i++){
-              nums[i] += run_sum[i-1];
-              run_sum.push_back(nums[i]);
+             
+              run_sum.push_back(nums[i] + run_sum[i-1]);
         }
         return run_sum;
     }
