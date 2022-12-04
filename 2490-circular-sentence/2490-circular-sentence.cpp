52ff4611a6fe5b2d2f3795475b0a432363738593
Ayush Kr. Pathak <104672642+payush17@users.noreply.github.com>
2022-12-04 10:38:32 +0530

    Time: 0 ms (100.00%), Space: 6.7 MB (60.00%) - LeetHub

---
+++ 2490-circular-sentence/2490-circular-sentence.cpp
@@ -0,0 +1,26 @@
+class Solution
+{
+    public:
+        bool isCircularSentence(string s)
+        {
+       
+            int string_len = s.length();
+            if(string_len<2)return true;
+            if ((s[0]) != (s[string_len - 1]))
+                return false;
+
+            for (int i = 1; i < string_len; i++)
+            {
+                if (isspace(s[i]))
+                {
+                    char c1 = (s[i - 1]);
+                    char c2 = (s[i + 1]);
+
+                    if (c1 != c2)
+                        return false;
+                }
+            }
+
+            return true;
+        }
+};
