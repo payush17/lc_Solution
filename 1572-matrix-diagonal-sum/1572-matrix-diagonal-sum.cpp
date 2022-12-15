0b00fe690e5190316adbfc0a9abd3c9a8f6d5cea
Ayush Kr. Pathak <104672642+payush17@users.noreply.github.com>
2022-12-15 09:12:55 +0530

    Time: 14 ms (93.31%), Space: 11.2 MB (60.73%) - LeetHub

--- 1572-matrix-diagonal-sum/1572-matrix-diagonal-sum.cpp
+++ 1572-matrix-diagonal-sum/1572-matrix-diagonal-sum.cpp
@@ -3,16 +3,27 @@ class Solution
     public:
         int diagonalSum(vector<vector < int>> &mat)
         {
-            long diagonal_sum = 0;
-            int len = mat.size();
-            for (int i = 0; i < len; i++)
-            {
-                for (int j = 0; j < len; j++)
-                {
-                    if (i == j || i + j + 1 == len)
-                        diagonal_sum += mat[i][j];
-                }
+             long diagonal_sum = 0;
+             long pri_diagonalSum=0;
+             long scn_diagonalSum=0;
+             int len = mat.size();
+            // for (int i = 0; i < len; i++)
+            // {
+            //     for (int j = 0; j < len; j++)
+            //     {
+            //         if (i == j || i + j + 1 == len)
+            //             diagonal_sum += mat[i][j];
+            //     }
+            // }
+            // return diagonal_sum
+            
+            
+            for(int i=0;i<len;i++){
+                pri_diagonalSum += mat[i][i];
+                scn_diagonalSum += mat[i][len-i-1];
             }
-            return diagonal_sum;
+            diagonal_sum = pri_diagonalSum + scn_diagonalSum;
+            return len&1 == 1 ? diagonal_sum - mat[len/2][len/2] : diagonal_sum;
+            
         }
 };
