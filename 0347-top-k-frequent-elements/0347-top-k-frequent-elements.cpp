8793561d568c69d37c819539f7bfe103fd34514b
Ayush Kr. Pathak <104672642+payush17@users.noreply.github.com>
2022-12-17 16:34:46 +0530

    Time: 16 ms (92.40%), Space: 13.6 MB (74.81%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
@@ -1,27 +1,26 @@
-class Solution {
-public:
-    vector<int> topKFrequent(vector<int>& nums, int k) {
-        unordered_map<int,int> map;
-        vector<vector<int>> count(nums.size()+1);
-        vector<int> kfreq_ele;
-        int topele=0;
-        for(auto x:nums)
-            map[x]++;
-        
-        for(auto x:map){
-            count[x.second].push_back(x.first);
-        }
-        for(int i=nums.size(); i>0; i--){
-            if(count[i].size()!=0){
-                for(int j=0; j<count[i].size(); j++){
-                    kfreq_ele.push_back(count[i][j]);
-                    topele++;
-                }
+class Solution
+{
+    public:
+        vector<int> topKFrequent(vector<int> &nums, int k)
+        {
+            unordered_map<int, int> map;
+            priority_queue<pair<int, int>> pq;
+            vector<int> kfreq_ele;
+
+            for (auto x: nums)
+                map[x]++;
+
+            for (auto x: map)
+            {
+                pq.push({ x.second,
+                    x.first });
             }
-            if(topele==k) break;
+            while (k--)
+            {
+                kfreq_ele.push_back(pq.top().second);
+                pq.pop();
+            }
+
+            return kfreq_ele;
         }
-       
-        
-        return kfreq_ele;
-    }
 };
