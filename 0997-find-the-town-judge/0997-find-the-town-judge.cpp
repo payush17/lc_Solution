820540ca964272b10d57bfa86c20bd19cdedc6d4
Ayush Kr. Pathak <104672642+payush17@users.noreply.github.com>
2022-12-19 14:57:58 +0530

    Time: 227 ms (79.48%), Space: 69 MB (18.42%) - LeetHub

--- 0997-find-the-town-judge/0997-find-the-town-judge.cpp
+++ 0997-find-the-town-judge/0997-find-the-town-judge.cpp
@@ -1,17 +1,21 @@
-class Solution {
-public:
-    int findJudge(int n, vector<vector<int>>& trust) {
-        
-        vector<int> trusting(n+1,0);
-        vector<int>trusted(n+1,0);
-        for(auto t:trust){
-            trusting[t[0]]++;
-            trusted[t[1]]++;
+class Solution
+{
+    public:
+        int findJudge(int n, vector<vector < int>> &trust)
+        {
+
+            vector<int> trusting(n + 1, 0);
+            vector<int> trusted(n + 1, 0);
+            for (auto t: trust)
+            {
+                trusting[t[0]]++;
+                trusted[t[1]]++;
+            }
+            for (int i = 1; i < n + 1; i++)
+            {
+                if (trusting[i] == 0 && trusted[i] == n - 1)
+                    return i;
+            }
+            return -1;
         }
-        for(int i=1;i<n+1;i++){
-            if(trusted[i]==n-1 && trusting[i]==0)
-                return i;
-        }
-        return -1;
-    }
 };
