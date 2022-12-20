ac22fc0424863c9476dab5b82b7d236367518db1
Ayush Kr. Pathak <104672642+payush17@users.noreply.github.com>
2022-12-20 12:45:20 +0530

    Time: 265 ms (19.04%), Space: 96.3 MB (56.71%) - LeetHub

--- 0463-island-perimeter/0463-island-perimeter.cpp
+++ 0463-island-perimeter/0463-island-perimeter.cpp
@@ -1,40 +1,23 @@
 class Solution
 {
     public:
-
-        void exploreIsland(vector<vector < int>> &grid, int i, int j, int &islandPerimeter)
+        int islandPerimeter(vector<vector < int>> &grid)
         {
-            if (i < 0 || i >= grid.size() || j < 0 || j >= grid[0].size() || grid[i][j] == 0)
+           	//better than DFS 
+            int islandPerimeter = 0;
+            for (int i = 0; i < grid.size(); i++)
             {
-                islandPerimeter += 1;
-                return;
-            }
-            if (grid[i][j] == -1)
-                return;
-            if (grid[i][j] == 1)
-                grid[i][j] = -1;
-
-            exploreIsland(grid, i + 1, j, islandPerimeter);
-            exploreIsland(grid, i, j - 1, islandPerimeter);
-            exploreIsland(grid, i - 1, j, islandPerimeter);
-            exploreIsland(grid, i, j + 1, islandPerimeter);
-        }
-    int islandPerimeter(vector<vector < int>> &grid)
-    {
-        int n = grid.size();
-        int m = grid[0].size();
-        int islandPerimeter = 0;
-        for (int i = 0; i < n; i++)
-        {
-            for (int j = 0; j < m; j++)
-            {
-                if (grid[i][j] == 1)
+                for (int j = 0; j < grid[0].size(); j++)
                 {
-                    exploreIsland(grid, i, j, islandPerimeter);
-                    break;
+                    if (grid[i][j])
+                    {
+                        if (i - 1 < 0 || grid[i - 1][j] == 0) islandPerimeter++;
+                        if (i + 1 >= grid.size() || grid[i + 1][j] == 0) islandPerimeter++;
+                        if (j - 1 < 0 || grid[i][j - 1] == 0) islandPerimeter++;
+                        if (j + 1 >= grid[0].size() || grid[i][j + 1] == 0) islandPerimeter++;
+                    }
                 }
             }
+            return islandPerimeter;
         }
-        return islandPerimeter;
-    }
 };
