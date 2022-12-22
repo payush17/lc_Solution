f421e0e70e53a9c2e51d256e449092ff7c3237cd
Ayush Kr. Pathak <104672642+payush17@users.noreply.github.com>
2022-12-22 16:59:12 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (93.10%) - LeetHub

--- 1753-maximum-score-from-removing-stones/1753-maximum-score-from-removing-stones.cpp
+++ 1753-maximum-score-from-removing-stones/1753-maximum-score-from-removing-stones.cpp
@@ -3,21 +3,26 @@ class Solution
     public:
         int maximumScore(int a, int b, int c)
         {
-            priority_queue<int> pq;
-            pq.push(a);
-            pq.push(b);
-            pq.push(c);
-            int maxScore = 0;
-            for (;pq.size()>1; maxScore++)
-            {
+//             priority_queue<int> pq;
+//             pq.push(a);
+//             pq.push(b);
+//             pq.push(c);
+//             int maxScore = 0;
+//             for (;pq.size()>1; maxScore++)
+//             {
                 
-                int max1 = pq.top();
-                pq.pop();
-                int max2 = pq.top();
-                pq.pop();
-                if (--max1 > 0) pq.push(max1);
-                if (--max2 > 0) pq.push(max2);
-            }
-            return maxScore;
+//                 int max1 = pq.top();
+//                 pq.pop();
+//                 int max2 = pq.top();
+//                 pq.pop();
+//                 if (--max1 > 0) pq.push(max1);
+//                 if (--max2 > 0) pq.push(max2);
+//             }
+//             return maxScore;
+            
+        if(a+b<c)return a+b;
+        if(b+c<a)return b+c;
+        if(a+c<b)return a+c;
+        return (a+b+c)/2;
         }
 };
