69da39b3d42baf5dafd7f49833a533a02b56681c
Ayush Kr. Pathak <104672642+payush17@users.noreply.github.com>
2023-01-02 13:33:42 +0530

    Time: 255 ms (71.51%), Space: 124 MB (67.65%) - LeetHub

--- 2149-rearrange-array-elements-by-sign/2149-rearrange-array-elements-by-sign.cpp
+++ 2149-rearrange-array-elements-by-sign/2149-rearrange-array-elements-by-sign.cpp
@@ -1,22 +1,19 @@
-class Solution
-{
-    public:
-        vector<int> rearrangeArray(vector<int> &nums)
-        {
-           	// BF
-            vector<int> pos;
-            vector<int> neg;
-            vector<int> Posneg;
-            for (int i = 0; i < nums.size(); i++)
-            {
-                if (nums[i] > 0) pos.push_back(nums[i]);
-                else neg.push_back(nums[i]);
+class Solution {
+public:
+    vector<int> rearrangeArray(vector<int>& nums) {
+        vector<int> posNeg(nums.size(),0);
+        int posInd=0;
+        int negInd=1;
+        for(int i=0;i<nums.size();i++){
+            if(nums[i]>0){
+                posNeg[posInd]=nums[i];
+                posInd+=2;
             }
-            for (int i = 0; i < nums.size() / 2; i++)
-            {
-                Posneg.push_back(pos[i]);
-                Posneg.push_back(neg[i]);
+            if(nums[i]<0){
+                posNeg[negInd]=nums[i];
+                negInd+=2;
             }
-            return Posneg;
         }
+        return posNeg;
+    }
 };
