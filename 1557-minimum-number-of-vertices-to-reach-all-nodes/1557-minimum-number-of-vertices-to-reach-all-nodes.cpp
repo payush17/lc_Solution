775a7eaad361bd9d403855976aa6a442d3b2f881
Ayush Kr. Pathak <104672642+payush17@users.noreply.github.com>
2023-01-04 19:39:10 +0530

    Time: 661 ms (34.78%), Space: 94.4 MB (64.48%) - LeetHub

--- 1557-minimum-number-of-vertices-to-reach-all-nodes/1557-minimum-number-of-vertices-to-reach-all-nodes.cpp
+++ 1557-minimum-number-of-vertices-to-reach-all-nodes/1557-minimum-number-of-vertices-to-reach-all-nodes.cpp
@@ -1,14 +1,21 @@
-class Solution {
-public:
-    vector<int> findSmallestSetOfVertices(int n, vector<vector<int>>& edges) {
-        vector<int> degree(n);vector<int>reach;
-        for(auto x:edges){
-            degree[x[1]]++;
+class Solution
+{
+    public:
+        vector<int> findSmallestSetOfVertices(int n, vector<vector < int>> &edges)
+        {
+            vector<int> reach;
+            vector<int> indeg(n);
+            for (int i = 0; i < edges.size(); i++)
+            {
+                indeg[edges[i][1]]++;
+            }
+            for (int i = 0; i < n; i++)
+            {
+                if (indeg[i] == 0)
+                {
+                    reach.push_back(i);
+                }
+            }
+            return reach;
         }
-        for(int i=0;i<n;i++){
-            if(!degree[i])
-                reach.push_back(i);
-        }
-        return reach;
-    }
 };
