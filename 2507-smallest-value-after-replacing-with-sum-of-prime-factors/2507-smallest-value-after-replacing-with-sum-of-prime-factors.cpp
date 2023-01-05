67516315cac409affe45a43a1487d2c6ec1e3ed2
Ayush Kr. Pathak <104672642+payush17@users.noreply.github.com>
2023-01-05 21:45:35 +0530

    Time: 2 ms (76.73%), Space: 6.1 MB (40.51%) - LeetHub

---
+++ 2507-smallest-value-after-replacing-with-sum-of-prime-factors/2507-smallest-value-after-replacing-with-sum-of-prime-factors.cpp
@@ -0,0 +1,35 @@
+class Solution
+{
+    public:
+        bool check = true;
+    int primSum(int n)
+    {
+        int sum = 0;
+        for (int i = 2; i * i <= n; i++)
+        {
+            while (n % i == 0)
+            {
+                sum += i;
+                n /= i;
+            }
+        }
+        if (n > 1) sum += n;
+        if (sum == n)
+            check = false;
+        return sum;
+    }
+
+    int smallestValue(int n)
+    {
+        int ans = INT_MAX;
+        if(n==4)return 4;
+        int ps = n;
+        while (check)
+        {
+            ps = primSum(ps);
+
+            ans = min(ans, ps);
+        }
+        return ans;
+    }
+};
