e9380573e70f8148a763684d0ee02716ffe81011
Ayush Kr. Pathak <104672642+payush17@users.noreply.github.com>
2023-01-16 11:40:28 +0530

    Time: 136 ms (56.69%), Space: 74.8 MB (6.22%) - LeetHub

---
+++ 1299-replace-elements-with-greatest-element-on-right-side/1299-replace-elements-with-greatest-element-on-right-side.cpp
@@ -0,0 +1,21 @@
+class Solution
+{
+    public:
+        vector<int> replaceElements(vector<int> &arr)
+        {
+            priority_queue<int> pq;
+            int n=arr.size();
+            vector<int> maxx(arr.size());
+            maxx[n-1]=-1;
+            pq.push(arr[n-1]);
+            
+            for(int i=n-2;i>=0;i--){
+                
+                maxx[i]=pq.top();
+                pq.push(arr[i]);
+               
+            }
+           
+            return maxx;
+        }
+};
